ec692fc13e118b55c30e47b1aa6d2bb7e1e470ea
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-28 16:37:01 +0900

    Commit procedual codes

---
+++ procedual/fizzbuzz.js
@@ -0,0 +1,27 @@
+"use strict;"
+
+// const oneToHundred = [...Array(100).keys()].map(num => ++num);
+//
+// oneToHundred.forEach(num => {
+//   if (num % 3 === 0 && num % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (num % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (num % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(`${num}`);
+//   }
+// })
+//
+// oneToHundred.forEach(num => {
+//   if (num % 3 === 0) {
+//     num % 5 === 0 ? console.log("FizzBuzz") : console.log("Fizz");
+//   } else {
+//     num % 5 === 0 ? console.log("Buzz") : console.log(`${num}`);
+//   }
+// })
+//
+// oneToHundred.forEach(num => {
+//   num % 3 === 0 ? (num % 5 === 0 ? console.log("FizzBuzz") : console.log("Fizz")) : num % 5 === 0 ? console.log("Buzz") : console.log(`${num}`);
+// })
