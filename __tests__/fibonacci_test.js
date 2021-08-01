635692890505eb99f37f19889765dc5eccb99690
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-08-02 07:47:59 +0900

    Commit Fibonacci

---
+++ __tests__/fibonacci_test.js
@@ -0,0 +1,11 @@
+"use strict;"
+
+import {
+  fibonacci
+} from "../lib/fibonacci";
+
+test("fizzBuzzIfVer", () => {
+  expect(fibonacci(0, 20)).toStrictEqual(
+    [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
+  );
+});
---
+++ exec/fibonacci_exec.js
@@ -0,0 +1,7 @@
+"use strict;"
+
+import {
+  fibonacci
+} from "../lib/fibonacci";
+
+console.log(fibonacci(0, 20));
