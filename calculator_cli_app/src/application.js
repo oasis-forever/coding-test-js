b83f942813661ef141f273bdf75d2e3cf38a5b01
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-12-27 22:54:27 +0900

    Commit calculation application

---
+++ calculator_cli_app/src/application.js
@@ -0,0 +1,33 @@
+import { CalculationQuery } from './queies/calculation_query';
+import { validate } from './validations/args_validation';
+
+const Application = class {
+  constructor(args) {
+    this.args_size        = args.length;
+    this.seed             = args[0];
+    this.n                = args[args.length -1];
+    this.calulation_query = null;
+  }
+
+  run() {
+    validate;
+
+    const result = this._calulation_query().f(n)
+    console.log(result)
+  }
+
+  // private
+
+  _calulation_query() {
+    if (this.calulation_query === null) {
+      this.calulation_query = new CalculationQuery(this.seed);
+      return this.calulation_query;
+    } else {
+      return this.calulation_query;
+    }
+  }
+}
+
+export {
+  Application
+};
