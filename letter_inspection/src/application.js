2e54bb5f4d536c4a352360d9769df59b96771071
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-12-27 14:08:51 +0900

    Make function private

--- letter_inspection/src/application.js
+++ letter_inspection/src/application.js
@@ -6,12 +6,14 @@ const Application = class {
     this.str_2 = str_2
   }
 
-  sort_string(str) {
-    return str.split("").sort().join("");
+  exactly_equal_size_and_included() {
+    return this._sort_string(this.str_1) === this._sort_string(this.str_2);
   }
 
-  exactly_equal_size_and_included() {
-    return this.sort_string(this.str_1) === this.sort_string(this.str_2);
+  // private
+
+  _sort_string(str) {
+    return str.split("").sort().join("");
   }
 }
 
