debe09451ee76bdc16d142780d3a8c4d9f69cf64
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-12-27 22:54:47 +0900

    Commit calculation validator

---
+++ calculator_cli_app/src/validations/args_validation.js
@@ -0,0 +1,19 @@
+const validate = (args_size, seed, n) => {
+  if (args_size > 2) {
+    console.log("Too many arguments");
+    process.exit(1);
+  } else if (args_size === 0) {
+    console.log("No argument provided");
+    process.exit(1);
+  } else if (args_size === 1) {
+    console.log("Provide both seed and n");
+    process.exit(1);
+  } else if (!typeof seed !== "string" || isNaN(n)) {
+    console.log("Provide seed as string and n as number");
+    process.exit(1);
+  }
+}
+
+export {
+  validate
+};
