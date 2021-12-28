790a594f43fb93a42eecc24fc48e54fe2dd81d51
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-12-28 11:34:00 +0900

    Remove ! operator

--- calculator_cli_app/src/validations/args_validation.js
+++ calculator_cli_app/src/validations/args_validation.js
@@ -8,7 +8,7 @@ const validate = (args_size, seed, n) => {
   } else if (args_size === 1) {
     console.log("Provide both seed and n");
     process.exit(1);
-  } else if (!typeof seed !== "string" || isNaN(n)) {
+  } else if (typeof seed !== "string" || isNaN(n)) {
     console.log("Provide seed as string and n as number");
     process.exit(1);
   }
