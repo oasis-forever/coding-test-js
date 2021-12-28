c9860f7d841b5ad3718b2a1cc0cbd75a1c45361e
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-12-28 12:22:05 +0900

    Commit test for calculation_query

---
+++ calculator_cli_app/__tests__/queries/calculation_query_test.js
@@ -0,0 +1,20 @@
+"use strict;"
+
+import {
+  CalculationQuery
+} from "../../src/queries/calculation_query";
+
+const calulation_query = new CalculationQuery("foo");
+
+test("test_f_with_zero", () => {
+  expect(calulation_query.f(0)).toBe(1);
+});
+
+test("test_f_with_two", () => {
+  expect(calulation_query.f(2)).toBe(2);
+});
+
+test("test_f_with_four", () => {
+  // fixme: NaN returned
+  expect(calulation_query.f(4)).toBe(148);
+});
