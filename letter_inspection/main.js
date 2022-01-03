38a0bb60f7ed28382ab094b9e4537c8976230a30
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2022-01-03 19:49:54 +0900

    Add underscore to each function

--- letter_inspection/main.js
+++ letter_inspection/main.js
@@ -4,14 +4,14 @@ import {
   LetterInspector
 } from "./src/letter_inspector";
 
-const str1 = 'hogefoobar';
-const str2 = 'abefghooor';
-const str3 = 'hoge';
-const str4 = 'piyopoopee';
-const pattern1 = new LetterInspector(str1, str2);
-const pattern2 = new LetterInspector(str1, str3);
-const pattern3 = new LetterInspector(str1, str4);
+const str_1     = 'hogefoobar';
+const str_2     = 'abefghooor';
+const str_3     = 'hoge';
+const str_4     = 'piyopoopee';
+const pattern_1 = new LetterInspector(str_1, str_2);
+const pattern_2 = new LetterInspector(str_1, str_3);
+const pattern_3 = new LetterInspector(str_1, str_4);
 
-console.log(pattern1.exactly_equal_size_and_included());
-console.log(pattern2.exactly_equal_size_and_included());
-console.log(pattern3.exactly_equal_size_and_included());
+console.log(pattern_1.exactly_equal_size_and_included());
+console.log(pattern_2.exactly_equal_size_and_included());
+console.log(pattern_3.exactly_equal_size_and_included());
